47da01b4286d1aabe734c222b3faa9774839cb31
ABDO-SEMLALI <e.abdelkarimsemlali@gmail.com>
2024-01-12 16:39:39 +0100

    more changes

--- notificationManager.groovy
+++ notificationManager.groovy
@@ -1,15 +1,17 @@
-#!groovy
-
 package workflowlibs.manager;
 
 import groovy.text.StreamingTemplateEngine
 
 /**
- * This method returns a string with the template filled with groovy variables
+ * This method returns a string with the template filled with Groovy variables
  */
 def emailTemplate(params) {
-
     def fileName = "email.html.groovy"
+    
+    // Instantiate StreamingTemplateEngine
+    def engine = new StreamingTemplateEngine()
+
+    // Use Java's Files class to read the file with UTF-8 encoding
     def fileContentsBytes = libraryResource(fileName).read()
     def fileContents = new String(fileContentsBytes, "UTF-8")
 
@@ -17,21 +19,17 @@ def emailTemplate(params) {
 }
 
 /**
- * This method send an email generated with data from Jenkins
+ * This method sends an email generated with data from Jenkins
  * @param buildStatus String with job result
  * @param emailRecipients Array with emails: emailRecipients = []
  */
-
-
 def notifyEmail(buildStatus, emailRecipients) {
-
     try {
-
         def icon = "✅"
         def statusSuccess = true
         def hasArtifacts = true
 
-        if(buildStatus != "SUCCESSFUL") {
+        if (buildStatus != "SUCCESSFUL") {
             icon = "❌"
             statusSuccess = false
             hasArtifacts = false
@@ -43,16 +41,16 @@ def notifyEmail(buildStatus, emailRecipients) {
             "statusSuccess" :   statusSuccess,
             "hasArtifacts"  :   hasArtifacts,
             "downloadUrl"   :   "www.downloadurl.com"
-        ]);
+        ])
 
-        mail (to: emailRecipients.join(","),
-            subject: "${icon} [ ${env.JOB_NAME} ] [${env.BUILD_NUMBER}] - ${buildStatus} ",
-            body: body,
-            mimeType: 'text/html'
-        );
-
-    } catch (e){
+        mail(to: emailRecipients.join(","),
+             subject: "${icon} [ ${env.JOB_NAME} ] [${env.BUILD_NUMBER}] - ${buildStatus} ",
+             body: body,
+             mimeType: 'text/html'
+        )
+    } catch (e) {
         println "ERROR SENDING EMAIL ${e}"
     }
 }
+
 return this;
